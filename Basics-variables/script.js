4755a4e6dd9481f646a0b54887acb15620cbc48e
Milcik1985 <148201145+Milcik1985@users.noreply.github.com>
2024-02-23 12:10:21 +0200

    Add files via upload

---
+++ Basics-variables/script.js
@@ -0,0 +1,44 @@
+let admin, vardas;
+
+vardas = "Jonas";
+
+admin = vardas;
+
+alert(admin);
+
+console.log(vardas);
+
+
+let ourPlanetName = "Earth";
+
+console.log(ourPlanetName);
+
+let userName = "Jonas";
+
+const text1 = "labas";
+
+const text2 = "labas";
+
+const result = text1 === text2;
+
+console.log(result);
+
+const number = 10;
+
+const text = "10";
+
+console.log(number != text);
+
+console.log (number==text);
+
+console.log(number===text);
+
+const a = 15;
+const b = 7;
+alert (a % b);
+console.log(a % b);
+
+const currentBalance = 50;
+const transaction = -50;
+const result = currentBalance + transaction >= 0;
+console.log(result);
